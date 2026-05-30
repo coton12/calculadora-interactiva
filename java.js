936cd13ad5b395260c3deed3fca3d5dd91a9daf6
coton12 <143169100+coton12@users.noreply.github.com>
2026-05-30 02:53:38 -0600

    Update java.js

--- java.js
+++ java.js
@@ -1,16 +1,13 @@
 
+ function calcularMultiplicacion() {
+         const n1 = parseFloat(document.getElementById('multNum1').value);
+ const n2 = parseFloat(document.getElementById('multNum2').value);
+ const rContainer = document.getElementById('resMult');
 
-    // Lógica para resolver la suma
-    function calcularSuma() {
-            // El parseFloat es clave, si no JavaScript junta los números como texto (ej: 2 + 2 = 22)
-            const n1 = parseFloat(document.getElementById('sumNum1').value);
-    const n2 = parseFloat(document.getElementById('sumNum2').value);
-    const rContainer = document.getElementById('resSuma');
+ if (isNaN(n1) || isNaN(n2)) {
+     rContainer.textContent = "Por favor, llena ambos campos.";
+ return;
+         }
 
-    if (isNaN(n1) || isNaN(n2)) {
-        rContainer.textContent = "Por favor, llena ambos campos.";
-    return;
-            }
-
-    rContainer.textContent = `Resultado: ${n1 * n2}`;
-}
+ rContainer.textContent = `Resultado: ${n1 * n2}`;
+     }
